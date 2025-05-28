4ae68d3518c264d25ccd0729000b0404f644b8a3
Sai Manikanteswara Reddy Reddybathuni <saimanikanteswarareddyreddybathuni@MacBookAir.lan>
2025-05-28 14:41:01 -0500

    conversation

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-PERSON1=Modi
-PERSON2=Musk
+PERSON1= Modi
+PERSON2= Musk
 
 echo " $PERSON1 : Hello $PERSON2, How are you doing?"
 echo  "$PERSON2: Hi $PERSON1, I am doing good, how are you?"
