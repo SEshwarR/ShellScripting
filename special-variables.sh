78d410765bfb69a35c01cd18746d880480bc985f
Sai Manikanteswara Reddy Reddybathuni <saimanikanteswarareddyreddybathuni@MacBookAir.lan>
2025-05-29 11:34:34 -0500

    specialvariables

--- special-variables.sh
+++ special-variables.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo " All variables passed to the script: $@"
+echo " no of variables: $#"
+echo " Script name: $0"
+echo "current directory : $PWD"
+echo " user running the script : $USER"
+echo " Home directory of the user: $HOME"
+echo " PID of the script: $$"
+sleep 10 &
+echo " Pid of the background process: $!"
