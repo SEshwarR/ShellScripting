31564dffe0ab6f5d352012b50a0ee1e0da150f56
Sai Manikanteswara Reddy Reddybathuni <saimanikanteswarareddyreddybathuni@MacBookAir.lan>
2025-06-19 12:27:51 -0500

    forloop-2

--- forloop2.sh
+++ forloop2.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("python","nginx","MySql","httpd")
+PACKAGES=("python" "nginx" "MySql" "httpd")
 
 mkdir -p $LOGS_FOLDER
 
