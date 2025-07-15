1873b85f713299a00b2ed833ba08ac0ff14e1f66
SEshwarR <saireddybathuni33@gmail.com>
2025-07-15 11:24:43 -0500

    s-1 and s-2

---
+++ script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+GREETINGS='HEllo , i am script-1'
+echo "$V1"
+echo "PID of script-1 is :$$"
+
+source ./script-2.sh
---
+++ script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+V1="Helo this is s-2 variable"
+echo "$GREETINGS"
+echo "PID of script-2 is: $$"
